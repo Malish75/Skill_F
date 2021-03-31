4dd3e849769e2d0a8b2fb4770aed473c0ffd0324
Malish75 <asmal75@bk.ru>
2021-03-31 17:02:39 +0300

    пробы Botstrap

--- module_D/tutorial/tutorial/settings.py
+++ module_D/tutorial/tutorial/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,9 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'fpages',
 ]
-
-SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -123,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SITE_ID = 1
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
