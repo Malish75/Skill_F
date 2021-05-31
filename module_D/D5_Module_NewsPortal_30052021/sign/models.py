39355d940f43e0a0f01be69a1884061928ad54f1
Malish75 <asmal75@bk.ru>
2021-05-31 15:23:44 +0300

    добавление в группу common при регистрации

--- module_D/D5_Module_NewsPortal_30052021/sign/models.py
+++ module_D/D5_Module_NewsPortal_30052021/sign/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django import forms
-
+from allauth.account.forms import SignupForm
+from allauth.account.signals import user_signed_up
+from django.dispatch import receiver
 
 class BaseRegisterForm(UserCreationForm):
     email = forms.EmailField(label = "Email")
@@ -17,3 +19,17 @@ class BaseRegisterForm(UserCreationForm):
                   "password1",
                   "password2",
                   )
+
+
+class BasicSignupForm(SignupForm):
+
+    def save(self, request):
+        user = super(BasicSignupForm, self).save(request)
+        basic_group = Group.objects.get(name='Common')
+        basic_group.user_set.add(user)
+        return user
+
+@receiver(user_signed_up)
+def user_signed_up_(request, user, **kwargs):
+    Group.objects.get(name='Common').user_set.add(user)
+    user.save()
