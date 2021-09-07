a94fe3a7edee6de874dca513cb4c2f3ca022b0e9
Malish75 <asmal75@bk.ru>
2021-09-07 14:08:24 +0300

    добавил D13_Module_loggs

---
+++ module_D/D13_Module_loggs/newapp/filters.py
@@ -0,0 +1,31 @@
+from django_filters import FilterSet
+from .models import Post
+from django.forms import DateInput
+import django_filters
+
+
+class PostFilter(FilterSet):
+    title = django_filters.CharFilter(field_name='title',
+                                      lookup_expr='icontains',
+                                      label='По заголовку')
+
+    content_new_post = django_filters.CharFilter(field_name='content_new_post',
+                                      lookup_expr='icontains',
+                                      label='По тексту')
+
+
+    datetime = django_filters.DateFilter(field_name='time_in_new_post',
+                                         widget=DateInput(attrs={'type': 'date'}),
+                                         lookup_expr='gt', label='Не позднее')
+
+    # Здесь в мета классе надо предоставить модель и указать поля по которым будет фильтроваться (т.е. подбираться) информация
+    class Meta:
+        model = Post
+        fields = (
+            'title',
+            'content_new_post'
+            'datetime',
+        )
+        fields = {
+            'author': ['exact'],
+        }
